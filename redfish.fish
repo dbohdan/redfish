1ce02a818137386fbab39e9ced9ce25007fa4415
D. Bohdan <dbohdan@dbohdan.com>
2023-03-31 08:12:38 +0000

    feat: handle empty args; check arg count

--- redfish.fish
+++ redfish.fish
@@ -1,27 +1,54 @@
 #! /usr/bin/env fish
 
-function redfish-write
-    redis-cli del $argv[1]
-    redis-cli rpush $argv[1] (string escape $argv[2..])
+function redfish-write --argument-names key
+    set --local argc (count $argv)
+    if test $argc -eq 0
+        return 1
+    end
+
+    redis-cli del $key >/dev/null
+    if test $argc -eq 1
+        return
+    end
+
+    set --local rpushed (redis-cli rpush $key (string escape $argv[2..]))
+    test "$rpushed" -eq (count $argv[2..])
 end
 
-function redfish-read --no-scope-shadowing -a __redfish_var __redfish_key
+function redfish-read --no-scope-shadowing --argument-names __redfish_var __redfish_key
+    if test (count $argv) -ne 2
+        return 1
+    end
+
+    set $__redfish_var
+    if test "$(redis-cli llen $__redfish_key)" -eq 0
+        return
+    end
+
     # Do not unescape the results as a whole to prevent values from
     # being split on newlines.
     for __redfish_value in (redis-cli lrange $__redfish_key 0 -1)
-        set -a $__redfish_var "$(string unescape $__redfish_value)"
+        set --append $__redfish_var "$(string unescape $__redfish_value)"
     end
 
-    set -e __redfish_key __redfish_value __redfish_var
+    set --erase __redfish_key __redfish_value __redfish_var
 end
 
-function test-me
-    set --local a foo1\nfoo2 bar βαζ
+function redfish-run-tests
+    set --local initial foo1\nfoo2 bar βαζ
     set --local key fish-redfish-test
-    redfish-write $key $a
-    set -l got
+    redfish-write $key $initial
+    set --local got
     redfish-read got $key
-    set -s got
+    test "$initial" = "$got" || return 1
+
+    redfish-write $key uno
+    redfish-read got $key
+    test (count $got) -eq 1 || return 1
+
+    redfish-write $key
+    redfish-read got $key
+    test (count $got) -eq 0 || return 1
 end
 
-test-me
+redfish-run-tests
