05ff9feb2db5ea679a8306434bd38e17d3b3919f
D. Bohdan <dbohdan@dbohdan.com>
2023-05-02 15:36:01 +0000

    build(install,): add install.fish

---
+++ install.fish
@@ -0,0 +1,12 @@
+#! /usr/bin/env fish
+
+cd "$(path dirname "$(status filename)")"
+
+set --local src redfish.fish
+set --local dst $__fish_config_dir/conf.d/redfish.fish
+
+if not cp $src $dst
+    return 1
+end
+
+printf 'copied "%s" to "%s"\n' $src $dst
