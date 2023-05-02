94f5d5da2b4304738d3b8bbfaf92a885be9404ca
D. Bohdan <dbohdan@dbohdan.com>
2023-05-02 18:56:06 +0000

    build(install): source after installing

--- install.fish
+++ install.fish
@@ -8,5 +8,9 @@ set --local dst $__fish_config_dir/conf.d/redfish.fish
 if not cp $src $dst
     return 1
 end
-
 printf 'copied "%s" to "%s"\n' $src $dst
+
+if not source $dst
+    return 1
+end
+printf 'sourced "%s"\n' $dst
