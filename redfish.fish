06142bfe91ef72efbbdb6ff42f6bfbfd89391a34
D. Bohdan <dbohdan@dbohdan.com>
2023-05-02 13:43:16 +0000

    feat: add func "redfish"; "hide" funcs w/ "__"
    Change variable prefix from "_" to "__".

--- redfish.fish
+++ redfish.fish
@@ -9,34 +9,36 @@
 # * redis-cli(1);
 # * a Redis server (local by default).
 
-if not set --query _redfish_key_prefix
-    set --global _redfish_key_prefix redfish:
+if not set --query __redfish_key_prefix
+    set --global __redfish_key_prefix __redfish:
 end
-if not set --query _redfish_redis_cli_args
-    set --global _redfish_redis_cli_args
-end
-if not set --query _redfish_run_tests
-    set --global _redfish_run_tests 0
+if not set --query __redfish_redis_cli_args
+    set --global __redfish_redis_cli_args
 end
 
-function redfish-key --argument-names key
+function redfish --no-scope-shadowing
+    set --global __redfish_cmd __redfish_(string replace --all - _ $argv[1])
+    $__redfish_cmd $argv[2..]
+end
+
+function __redfish_key --argument-names key
     argparse --min-args 1 --max-args 1 -- $argv
     or return
 
-    echo -n $_redfish_key_prefix$key
+    echo -n $__redfish_key_prefix$key
 end
 
-function redfish-redis
-    redis-cli $_redfish_redis_cli_args $argv
+function __redfish_redis
+    redis-cli $__redfish_redis_cli_args $argv
 end
 
-function redfish-delete --argument-names key
+function __redfish_delete --argument-names key
     argparse --min-args 1 --max-args 1 -- $argv
     or return
 
-    set key (redfish-key $key)
+    set key (__redfish_key $key)
 
-    test "$(redfish-redis del $key)" -eq 1
+    test "$(__redfish_redis del $key)" -eq 1
     # The following `or return` statement does nothing. While currently
     # useless, it will become necessary for corrent error handling if we add
     # commands below it. Our stylistic choice is to have these returns from
@@ -44,72 +46,76 @@ function redfish-delete --argument-names key
     or return
 end
 
-function redfish-exists --argument-names key
+function __redfish_exists --argument-names key
     argparse --min-args 1 --max-args 1 -- $argv
     or return
 
-    set key (redfish-key $key)
+    set key (__redfish_key $key)
 
-    test "$(redfish-redis exists $key)" -eq 1
+    test "$(__redfish_redis exists $key)" -eq 1
     or return
 end
 
-function redfish-read --argument-names key
+function __redfish_read --argument-names key
     argparse --min-args 1 --max-args 1 -- $argv
     or return
 
-    set key (redfish-key $key)
+    set key (__redfish_key $key)
 
-    string unescape "$(redfish-redis get $key)"
+    string unescape "$(__redfish_redis get $key)"
     or return
 end
 
-function redfish-read-list --no-scope-shadowing --argument-names _redfish_var _redfish_key
+function __redfish_read_list --no-scope-shadowing --argument-names __redfish_dst_var __redfish_src_key
     argparse --min-args 2 --max-args 2 -- $argv
     or return
 
-    set _redfish_key (redfish-key $_redfish_key)
-    set $_redfish_var
+    set __redfish_src_key (__redfish_key $__redfish_src_key)
+    set $__redfish_dst_var
 
-    test "$(redfish-redis llen $_redfish_key)" -eq 0
+    test "$(__redfish_redis llen $__redfish_src_key)" -eq 0
     and return
 
     # Do not unescape the results as a whole to prevent values from
     # being split on newlines.
-    set --local _redfish_list (redfish-redis lrange $_redfish_key 0 -1)
+    set --local __redfish_list (__redfish_redis lrange $__redfish_src_key 0 -1)
     or return
 
-    for _redfish_value in $_redfish_list
-        set --append $_redfish_var "$(string unescape $_redfish_value)"
+    for __redfish_value in $__redfish_list
+        set --append $__redfish_dst_var "$(string unescape $__redfish_value)"
         or return
     end
 
-    set --erase _redfish_key _redfish_list _redfish_value _redfish_var
+    set --erase \
+        __redfish_dst_var \
+        __redfish_list \
+        __redfish_src_key \
+        __redfish_value
 end
 
-function redfish-write --argument-names key value
+function __redfish_write --argument-names key value
     argparse --min-args 2 --max-args 2 -- $argv
     or return
 
-    set key (redfish-key $key)
+    set key (__redfish_key $key)
 
-    redfish-redis set $key $value >/dev/null
+    __redfish_redis set $key $value >/dev/null
     or return
 end
 
-function redfish-write-list --argument-names key
+function __redfish_write_list --argument-names key
     argparse --min-args 1 -- $argv
     or return
 
-    set key (redfish-key $key)
+    set key (__redfish_key $key)
 
-    redfish-redis del $key >/dev/null
+    __redfish_redis del $key >/dev/null
     or return
 
     test (count $argv) -eq 1
     and return
 
-    set --local rpushed (redfish-redis rpush $key (string escape $argv[2..]))
+    set --local rpushed (__redfish_redis rpush $key (string escape $argv[2..]))
     or return
 
     test "$rpushed" -eq (count $argv[2..])
--- tests.fish
+++ tests.fish
@@ -8,43 +8,43 @@ source redfish.fish
 begin
     set --local initial foo1\nfoo2 bar βαζ
     set --local key fish-redis-test
-    redfish-write-list $key $initial
+    redfish write-list $key $initial
     set --local got
-    redfish-read-list got $key
+    redfish read-list got $key
     if not test "$initial" = "$got"
         printf '|%s| != |%s|\n' "$initial" "$got"
         return 101
     end
 
-    redfish-write-list $key uno
-    redfish-read-list got $key
+    redfish write-list $key uno
+    redfish read-list got $key
     test (count $got) -eq 1
     or return 102
 
-    redfish-write-list $key
-    redfish-read-list got $key
+    redfish write-list $key
+    redfish read-list got $key
     test (count $got) -eq 0
     or return 103
 
-    set got "$(redfish-read $key)"
+    set got "$(redfish read $key)"
     test "$got" = ''
     or return 104
 
-    redfish-write $key "$foo"
-    set got "$(redfish-read $key)"
+    redfish write $key "$foo"
+    set got "$(redfish read $key)"
     test "$got" = "$foo"
     or return 105
 
-    redfish-exists $key
+    redfish exists $key
     or return 106
 
-    redfish-delete $key
+    redfish delete $key
     or return 107
 
-    redfish-delete $key
+    redfish delete $key
     and return 108
 
-    redfish-exists $key
+    redfish exists $key
     and return 109
 
     return 0
